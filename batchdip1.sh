6b49da7b468ae6d4f9518df98d781cb093e59c3e
Avra98 <69806857+Avra98@users.noreply.github.com>
2023-03-07 15:12:07 -0500

    Add files via upload

---
+++ batchdip1.sh
@@ -0,0 +1,4 @@
+python -u inpaint.py Lena512rgb 0.1 20000 --IGR True --reg 0.3 --frac_img 0.8 --num_layers 3 --device_id 1 
+python -u inpaint.py Lena512rgb 0.1 20000 --IGR True --reg 0.3 --frac_img 0.5 --num_layers 3 --device_id 1 
+python -u inpaint.py Lena512rgb 0.1 20000 --IGR True --reg 0.3 --frac_img 0.2 --num_layers 3 --device_id 1 
+python -u inpaint.py Lena512rgb 0.1 20000 --IGR True --reg 0.3 --frac_img 0.1 --num_layers 3 --device_id 1 
